d685224ae86ab27f166d668663ca0ae692b7300f
Chang-Goo Kim <cgkim@astro.princeton.edu>
2023-06-20 16:47:48 -0400

    Set table cell width

--- src/coffeehost.py
+++ src/coffeehost.py
@@ -235,7 +235,7 @@ class Hosts(object):
         wstr = "|"
         for i in range(7):
             day = calendar.day_abbr[(i + calendar.SUNDAY) % 7]
-            wstr += f"<div style='max-width:100px'><p>{day}</p></div>|"
+            wstr += f"<div style='max-width:100px;width:100px'><p>{day}</p></div>|"
         fp.write(wstr + "\n")
 
         wstr = "|"
