0d3065400899d825c7e5fa39b594783e32d43abf
Chang-Goo Kim <cgkim@astro.princeton.edu>
2024-05-21 11:01:07 -0400

    Update 2024 Fall hosts

--- README.md
+++ README.md
@@ -3,6 +3,34 @@
 * Astro Coffee Program: https://coffee.astro.princeton.edu/astroph-coffee/papers/today
 * Astro Coffee Calendar: https://changgoo.github.io/AstroCoffee/intro.html
 
+# Create New Assignments
+
+This is a semi-automated script, requiring quite a bit of human intervention.
+
+First, update host information in `data/Coffee-Hosts-????.csv`.
+
+Second, run the assignment script:
+
+```sh
+python assign_hosts.py
+```
+
+Before running this script, a few manual updates maybe needed.
+* Update the output file name
+* Update the host list
+* Update the holiday file (including colloquium schedule)
+* Add restrictions
+* Add months to assign
+
+Third, run the script to create assignment emails and calendar.
+
+```sh
+python create_calendars.py
+```
+
+Finally, get responses and update host assignments. The calendar will be updated automatically.
+
+
 # Cron job setup
 
 I created a bash script at $HOME to call the `send_reminder.py` script.
--- src/assign_hosts.py
+++ src/assign_hosts.py
@@ -1,8 +1,20 @@
 from coffeehost import Hosts, Host, date, get_weekdays
 import pandas as pd
+import os
+import sys
 
-# read host file from the responses
-hostfile = "../data/Coffee-Hosts-2024.csv"
+base = os.path.dirname(__file__)
+
+# [Update this] Abort if the output file exists
+outfile = f"{base}/../data/hosts_2024_2.json"
+if os.path.isfile(outfile):
+    print(
+        "Warning: the host file exists. Please make sure to remove it or make a backup before running this script."
+    )
+    sys.exit()
+
+# [Update this] read host file from the responses
+hostfile = f"{base}/../data/Coffee-Hosts-2024.csv"
 hostlist = pd.read_csv(hostfile)
 
 # initialize host list
@@ -11,36 +23,37 @@ for n, e in zip(hostlist["Your Name"], hostlist["Email Address"]):
     h = Host(n, e)
     hosts[h.last.lower()] = h
 
-# add host specific restrictions
-hosts["secunda"].add_restriction(date(2024, 4, 1), date(2024, 4, 12))
-hosts["mohapatra"].add_restriction(date(2024, 1, 21), date(2024, 3, 2))
-hosts["mohapatra"].add_restriction(date(2024, 4, 15), date(2024, 4, 30))
-hosts["ward"].add_restriction(date(2024, 1, 1), date(2024, 4, 1))
-hosts["ward"].add_restriction(date(2024, 5, 15), date(2024, 5, 31))
-hosts["kempski"].add_restriction(date(2024, 2, 4), date(2024, 2, 24))
-hosts["strauss"].add_restriction(date(2024, 4, 4), date(2024, 4, 9))
-hosts["spitkovsky"].add_restriction(date(2024, 1, 5), date(2024, 1, 16))
-hosts["spitkovsky"].add_restriction(date(2024, 2, 29), date(2024, 3, 1))
-hosts["spitkovsky"].add_restriction(date(2024, 3, 25), date(2024, 3, 29))
-hosts["spitkovsky"].add_restriction(date(2024, 4, 8), date(2024, 4, 10))
-hosts["spitkovsky"].add_restriction(date(2024, 4, 15), date(2024, 4, 18))
-for wd in get_weekdays(2024, 2, exclude=["Monday", "Tuesday", "Wednesday", "Friday"]):
-    hosts["spitkovsky"].add_restriction(wd, wd)
-hosts["sur"].add_restriction(date(2024, 1, 1), date(2024, 2, 10))
-hosts["goodman"].add_restriction(date(2024, 4, 11), date(2024, 4, 12))
-hosts["kunz"].add_restriction(date(2024, 1, 1), date(2024, 1, 31))
+# [Update this] add host specific restrictions
+# for wd in get_weekdays(2024, 2, exclude=["Monday", "Tuesday", "Wednesday", "Friday"]):
+# hosts["spitkovsky"].add_restriction(wd, wd)
+hosts["kunz"].add_restriction(date(2024, 1, 1), date(2024, 8, 31))
+hosts["su"].add_restriction(date(2024, 1, 1), date(2024, 8, 31))
+hosts["greene"].add_restriction(date(2024, 1, 1), date(2024, 8, 31))
+hosts["modak"].add_restriction(date(2024, 1, 1), date(2024, 8, 31))
+hosts["sun"].add_restriction(date(2024, 8, 12), date(2024, 8, 16))
+hosts["sun"].add_restriction(date(2024, 9, 16), date(2024, 10, 4))
+hosts["ward"].add_restriction(date(2024, 6, 1), date(2024, 6, 30))
+hosts["ward"].add_restriction(date(2024, 7, 17), date(2024, 7, 21))
+hosts["ward"].add_restriction(date(2024, 8, 26), date(2024, 9, 6))
+hosts["secunda"].add_restriction(date(2024, 6, 3), date(2024, 6, 12))
+hosts["secunda"].add_restriction(date(2024, 6, 17), date(2024, 6, 25))
+hosts["secunda"].add_restriction(date(2024, 8, 21), date(2024, 12, 31))
+hosts["pan"].add_restriction(date(2024, 6, 1), date(2024, 8, 31))
+hosts["loudas"].add_restriction(date(2024, 6, 1), date(2024, 8, 10))
+hosts["strauss"].add_restriction(date(2024, 6, 6), date(2024, 6, 27))
+hosts["strauss"].add_restriction(date(2024, 7, 15), date(2024, 7, 26))
+hosts["bambic"].add_restriction(date(2024, 9, 1), date(2024, 10, 1))
 
+# [Update this] add dates to assign
+hosts.add_dates(get_weekdays(2024, 6))
+hosts.add_dates(get_weekdays(2024, 7))
+hosts.add_dates(get_weekdays(2024, 8))
+hosts.add_dates(get_weekdays(2024, 9))
+# hosts.add_dates(get_weekdays(2024, 5))
 
-# add dates to assign
-hosts.add_dates(get_weekdays(2024, 1))
-hosts.add_dates(get_weekdays(2024, 2))
-hosts.add_dates(get_weekdays(2024, 3))
-hosts.add_dates(get_weekdays(2024, 4))
-hosts.add_dates(get_weekdays(2024, 5))
-
-# exclude dates
+# [Update this] exclude dates
 Holidays = Hosts()
-Holidays.from_json("../data/holidays_2024.json")
+Holidays.from_json(f"{base}/../data/holidays_2024.json")
 dates = []
 for k in Holidays.hosts:
     for hd in Holidays[k].hostdate:
@@ -57,4 +70,4 @@ hosts.show()
 hosts.showlist()
 
 # store it to json file
-hosts.to_json("../data/hosts_2024_1.json")
+hosts.to_json(outfile)
