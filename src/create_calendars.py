b100754ad7f0e2db2653a4ac3479c7319227e391
Chang-Goo Kim <cgkim@astro.princeton.edu>
2025-09-28 16:50:03 -0400

    Updated 2025-3 assigment

--- src/create_calendars.py
+++ src/create_calendars.py
@@ -5,14 +5,14 @@ dirname = os.path.dirname(__file__)
 
 # initialize host list with assigned dates from json file
 newhosts = Hosts()
-newhosts.from_json(f"{dirname}/../data/holidays_2025.json")
+newhosts.from_json(f"{dirname}/../data/holidays_2026.json")
 # old holidays
-for year in ["2023", "2024"]:
+for year in ["2023", "2024", "2025"]:
     tmphosts = Hosts()
     tmphosts.from_json(f"{dirname}/../data/holidays_{year}.json")
     newhosts += tmphosts
 
-periods = ["2023_3", "2023_4", "2024_1", "2024_2", "2024_3", "2025_1", "2025_2"]
+periods = ["2023_3", "2023_4", "2024_1", "2024_2", "2024_3", "2025_1", "2025_2", "2025_3"]
 for period in periods:
     tmphosts = Hosts()
     tmphosts.from_json(f"{dirname}/../data/hosts_{period}.json")
@@ -24,12 +24,15 @@ tmphosts.show()
 
 # create calenders
 j = 0
-for i, month in enumerate(range(9, 0, -1)):
+for i, month in enumerate(range(1, 0, -1)):
+    newhosts.output_calendar(2026, month, num=j)
+    j += 1
+for i, month in enumerate(range(12, 0, -1)):
     newhosts.output_calendar(2025, month, num=j)
     j += 1
 # old calendar with fixed number
 for i, month in enumerate(range(12, 0, -1)):
-    newhosts.output_calendar(2024, month, num=0)
+    newhosts.output_calendar(2024, month, num=99)
 
 for i, month in enumerate(range(12, 6, -1)):
-    newhosts.output_calendar(2023, month, num=0)
+    newhosts.output_calendar(2023, month, num=99)
