9d0b6a3cbcf063ed8f874caa344d8f1fdadbb97e
Chang-Goo Kim <cgkim@astro.princeton.edu>
2026-01-15 12:10:16 -0500

    Cancel coffees

--- src/print_hosts.py
+++ src/print_hosts.py
@@ -5,7 +5,7 @@ dirname = os.path.dirname(__file__)
 
 # initialize recent host list
 hosts = Hosts()
-periods = ["2025_2"]
+periods = ["2025_3"]
 for period in periods:
     tmphosts = Hosts()
     tmphosts.from_json(f"{dirname}/../data/hosts_{period}.json")
